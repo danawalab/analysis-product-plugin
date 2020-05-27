ba8f9e815524f96c655b646aeed88a032e5f3f9f
Jaebaek Jeong <lupfeliz@gmail.com>
2020-05-27 21:49:10 +0900

    벌크색인 기능 추가 (임시)
    1. 벌크 색인 기능 추가.
    2. 파일권한용 보안규칙 파일 추가

--- src/main/java/com/danawa/search/analysis/product/ProductNameAnalysisAction.java
+++ src/main/java/com/danawa/search/analysis/product/ProductNameAnalysisAction.java
@@ -1,15 +1,27 @@
 package com.danawa.search.analysis.product;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.danawa.util.ContextStore;
 
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.SpecialPermission;
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.index.IndexRequestBuilder;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
@@ -39,9 +51,9 @@ public class ProductNameAnalysisAction extends BaseRestHandler {
 	private static final String CONTENT_TYPE_JSON = "application/json;charset=UTF-8";
 	private static final String BASE_URI = "/_product-name-analysis";
 	private static final ContextStore contextStore = ContextStore.getStore(AnalysisProductNamePlugin.class);
-	private static final String RELOAD_DICT = "reload-dict";
-	private static final Object ADD_DOCUMENT = "add-document";
-
+	private static final String ACTION_RELOAD_DICT = "reload-dict";
+	private static final String ACTION_ADD_DOCUMENT = "add-document";
+	private static final String ACTION_FULL_INDEX = "full-index";
 
 	@Inject
 	ProductNameAnalysisAction(Settings settings, RestController controller) {
@@ -53,17 +65,23 @@ public class ProductNameAnalysisAction extends BaseRestHandler {
 
 	@Override
 	public String getName() {
-		return "rest_handler_product-name-analysis";
+		return "rest_handler_product_name_analysis";
 	}
 
 	@Override
 	protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
 		final String action = request.param("action");
 
-		if (RELOAD_DICT.equals(action)) {
+		if (ACTION_RELOAD_DICT.equals(action)) {
 			reloadDictionary();
-		} else if (ADD_DOCUMENT.equals(action)) {
+		} else if (ACTION_ADD_DOCUMENT.equals(action)) {
 			addDocument(request, client);
+		} else if (ACTION_FULL_INDEX.equals(action)) {
+			SpecialPermission.check();
+			AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+				bulkIndex(request, client);
+				return null;
+			});
 		}
 
 		return channel -> {
@@ -83,12 +101,10 @@ public class ProductNameAnalysisAction extends BaseRestHandler {
 		}
 	}
 
-	public void addDocument(RestRequest request, NodeClient client) {
-
+	public void addDocument(final RestRequest request, final NodeClient client) {
 		try {
 			logger.debug("PARSING REST-BODY...");
-			String body = request.content().utf8ToString();
-			JSONObject jobj = new JSONObject(new JSONTokener(body));
+			JSONObject jobj = new JSONObject(new JSONTokener(request.content().utf8ToString()));
 			for (String key : jobj.keySet()) {
 				String value = jobj.optString(key, "");
 				logger.debug("PARSE VALUE {} = {}", key, value);
@@ -131,5 +147,80 @@ public class ProductNameAnalysisAction extends BaseRestHandler {
 		} catch (Exception e) {
 			logger.error("", e);
 		}
+	}
+
+	private void bulkIndex(final RestRequest request, final NodeClient client) {
+		final StringBuilder path = new StringBuilder();
+		final StringBuilder enc = new StringBuilder();
+		final StringBuilder indexName = new StringBuilder();
+		try {
+			JSONObject jobj = new JSONObject(new JSONTokener(request.content().utf8ToString()));
+			path.append(jobj.optString("path", ""));
+			enc.append(jobj.optString("enc", "euc-kr"));
+			indexName.append(jobj.optString("index", ""));
+		} catch (Exception e) {
+			logger.error("", e);
+		}
+
+		new Thread() {
+			@Override public void run() {
+				final File file = new File(String.valueOf(path));
+				if (!file.exists()) { 
+					logger.debug("FILE NOT FOUND : {}", path);
+					return; 
+				}
+				BufferedReader reader = null;
+				InputStream istream = null;
+				Pattern ptn = Pattern.compile("\\x5b[%]([a-zA-Z0-9]+)[%]\\x5d");
+				long time = System.currentTimeMillis();
+				try {
+					BulkRequestBuilder builder = client.prepareBulk();
+					Map<String, Object> source;
+					istream =  new FileInputStream(file);
+					reader = new BufferedReader(new InputStreamReader(istream, String.valueOf(enc)));
+					int count = 0;
+					logger.debug("PARSING FILE..");
+					for (String line; (line = reader.readLine()) != null; count++) {
+						Matcher mat = ptn.matcher(line);
+						String key = null;
+						int offset = 0;
+						source = new HashMap<>();
+						while (mat.find()) {
+							if (key != null) {
+								columnValue(source, key, line.substring(offset, mat.start()));
+							}
+							key = mat.group(1);
+							offset = mat.end();
+						}
+						columnValue(source, key, line.substring(offset));
+
+						builder.add(client.prepareIndex(String.valueOf(indexName), "_doc").setSource(source));
+						if (count > 0 && count % 50000 == 0) {
+							builder.execute().actionGet();
+							builder = client.prepareBulk();
+						}
+						if (count > 0 && count % 100000 == 0) {
+							logger.debug("{} ROWS FLUSHED! in {}ms", count, System.currentTimeMillis() - time);
+						}
+					}
+					builder.execute().actionGet();
+				} catch (Exception e) {
+					logger.error("", e);
+				} finally {
+					try { reader.close(); } catch (Exception ignore) { }
+				}
+			}
+		}.start();
+	}
+
+	private void columnValue(Map<String, Object> source, String key , String value) throws Exception {
+		if ("".equals(key)) {
+		} else if ("REGISTERDATE".equals(key)) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+			source.put(key, sdf.parse(value));
+		} else {
+			source.put(key, value);
+		}
+		logger.trace("ROW:{} / {}", key, value);
 	}
 }
