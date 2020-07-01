3ba6804b16d24a0af0fb046b2924b5e576a2620b
Jaebaek Jeong <lupfeliz@gmail.com>
2020-07-01 20:44:20 +0900

    Elasticsearch 쿼리파서를 통해 문자열쿼리와 플러그인 쿼리 통합 테스트

--- src/test/java/com/danawa/search/analysis/product/ProductNameAnalysisActionTest.java
+++ src/test/java/com/danawa/search/analysis/product/ProductNameAnalysisActionTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.danawa.search.analysis.dict.ProductNameDictionary;
 import com.danawa.search.index.DanawaSearchQueryBuilder;
@@ -14,8 +16,29 @@ import com.danawa.util.TestUtil;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
+import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.xcontent.DeprecationHandler;
+import org.elasticsearch.common.xcontent.LoggingDeprecationHandler;
+import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.elasticsearch.index.query.AbstractQueryBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.BoostingQueryBuilder;
+import org.elasticsearch.index.query.MatchPhraseQueryBuilder;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryStringQueryBuilder;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.index.query.SimpleQueryStringBuilder;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.index.query.TermsQueryBuilder;
+import org.elasticsearch.index.query.TermsSetQueryBuilder;
+import org.elasticsearch.index.query.WrapperQueryBuilder;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.plugins.SearchPlugin;
 import org.json.JSONObject;
 import org.json.JSONWriter;
 import org.junit.Before;
@@ -26,15 +49,17 @@ public class ProductNameAnalysisActionTest {
 	private static Logger logger = Loggers.getLogger(ProductNameAnalysisActionTest.class, "");
 	private static final ContextStore contextStore = ContextStore.getStore(AnalysisProductNamePlugin.class);
 
-	@Before public void init() {
-		TestUtil.setLogLevel(System.getProperty("LOG_LEVEL"), 
-			ProductNameTokenizer.class, 
-			ProductNameParsingRule.class,
-			ProductNameAnalysisFilter.class);
+	@Before
+	public void init() {
+		TestUtil.setLogLevel(System.getProperty("LOG_LEVEL"), ProductNameTokenizer.class, ProductNameParsingRule.class,
+				ProductNameAnalysisFilter.class);
 	}
 
-	@Test public void queryBuildTest() {
-		if (TestUtil.launchForBuild()) { return; }
+	@Test
+	public void queryBuildTest() {
+		if (TestUtil.launchForBuild()) {
+			return;
+		}
 		String text = "";
 		text = "테스트상품 ab12cd-12345";
 		text = "집업WAS1836ER27";
@@ -53,6 +78,131 @@ public class ProductNameAnalysisActionTest {
 		logger.debug("Q:{}", query.toString());
 		logger.debug("ANALYSIS:{}", analysis);
 		assertTrue(true);
+	}
+
+	@Test public void queryFromStringTest() throws Exception {
+		if (TestUtil.launchForBuild()) { return; }
+		logger.debug("PARSING..");
+		String source = "" +
+			"{ " +
+			"  \"query\": { " +
+			"    \"function_score\": { " +
+			"      \"query\": { " +
+			"        \"bool\": { " +
+			"          \"must\": [ " +
+			"            { " +
+			"              \"multi_match\": { " +
+			"                \"query\": \"바라쿠다\", " +
+			"                \"fields\": [ " +
+			"                  \"TOTALINDEX\", " +
+			"                  \"MODELWEIGHT^300000\", " +
+			"                  \"MAKERKEYWORD^200000\", " +
+			"                  \"BRANDKEYWORD^200000\", " +
+			"                  \"CATEGORYWEIGHT^100000\" " +
+			"                ], " +
+			"                \"operator\": \"OR\", " +
+			"                \"tie_breaker\": 0 " +
+			"              } " +
+			"            } " +
+			"          ], " +
+			"          \"filter\": [ " +
+			"            { " +
+			"              \"term\": { " +
+			"                \"DISPYN\": \"Y\" " +
+			"              } " +
+			"            }, " +
+			"            { " +
+			"              \"term\": { " +
+			"                \"PRICECOMPARISONSTOPYN\": \"N\" " +
+			"              } " +
+			"            }, " +
+			"            { " +
+			"              \"term\": { " +
+			"                \"CATEGORYDISPYN\": \"Y\" " +
+			"              } " +
+			"            } " +
+			"          ] " +
+			"        } " +
+			"      }, " +
+			"      \"functions\": [ " +
+			"        { " +
+			"          \"filter\": { " +
+			"            \"match\": { " +
+			"              \"SHOPPRODUCTCODE\": \"6562693705\" " +
+			"            } " +
+			"          }, " +
+			"          \"weight\": 12 " +
+			"        }, " +
+			"        { " +
+			"          \"filter\": { " +
+			"            \"match\": { " +
+			"              \"SHOPPRODUCTCODE\": \"P1207582350\" " +
+			"            } " +
+			"          }, " +
+			"          \"weight\": 40 " +
+			"        } " +
+			"      ], " +
+			"      \"boost_mode\": \"sum\" " +
+			"    } " +
+			"  }, " +
+			"  \"sort\": [ " +
+			"    { " +
+			"      \"_score\": { " +
+			"        \"order\": \"desc\" " +
+			"      }, " +
+			"      \"REGISTERDATE\": { " +
+			"        \"order\": \"desc\" " +
+			"      } " +
+			"    } " +
+			"  ] " +
+			"} ";
+
+		JSONObject json = new JSONObject(source);
+		source = json.optString("query");
+
+		DeprecationHandler dpHandler = LoggingDeprecationHandler.INSTANCE;
+
+		registerQuery(new SearchPlugin.QuerySpec<>(MatchQueryBuilder.NAME, MatchQueryBuilder::new,
+				MatchQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(MultiMatchQueryBuilder.NAME, MultiMatchQueryBuilder::new,
+				MultiMatchQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(MatchPhraseQueryBuilder.NAME, MatchPhraseQueryBuilder::new,
+				MatchPhraseQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(QueryStringQueryBuilder.NAME, QueryStringQueryBuilder::new,
+				QueryStringQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(BoostingQueryBuilder.NAME, BoostingQueryBuilder::new,
+				BoostingQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(BoolQueryBuilder.NAME, BoolQueryBuilder::new,
+				BoolQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(RangeQueryBuilder.NAME, RangeQueryBuilder::new,
+				RangeQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(WrapperQueryBuilder.NAME, WrapperQueryBuilder::new,
+				WrapperQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(FunctionScoreQueryBuilder.NAME, FunctionScoreQueryBuilder::new,
+				FunctionScoreQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(SimpleQueryStringBuilder.NAME, SimpleQueryStringBuilder::new,
+				SimpleQueryStringBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(TermsSetQueryBuilder.NAME, TermsSetQueryBuilder::new,
+				TermsSetQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(TermQueryBuilder.NAME, TermQueryBuilder::new,
+				TermQueryBuilder::fromXContent));
+		registerQuery(new SearchPlugin.QuerySpec<>(TermsQueryBuilder.NAME, TermsQueryBuilder::new,
+				TermsQueryBuilder::fromXContent));
+
+		NamedXContentRegistry registry = new NamedXContentRegistry(namedXContents);
+		XContentParser parser = JsonXContent.jsonXContent.createParser(registry, dpHandler, source);
+		long time = System.nanoTime();
+		QueryBuilder query = AbstractQueryBuilder.parseInnerQueryBuilder(parser);
+		time = System.nanoTime() - time;
+		logger.debug("Q:{}", query);
+		logger.debug("PARSING TAKES {} ms", ((int) Math.round(time * 100.0 / 1000000.0)) / 100.0);
+	}
+
+	private final List<NamedWriteableRegistry.Entry> namedWriteables = new ArrayList<>();
+	private final List<NamedXContentRegistry.Entry> namedXContents = new ArrayList<>();
+	private void registerQuery(SearchPlugin.QuerySpec<?> spec) {
+		namedWriteables.add(new NamedWriteableRegistry.Entry(QueryBuilder.class, spec.getName().getPreferredName(), spec.getReader()));
+		namedXContents.add(new NamedXContentRegistry.Entry(QueryBuilder.class, spec.getName(), (p, c) -> spec.getParser().fromXContent(p)));
 	}
 
 	@Test public void testAnalyzeDetail() {
